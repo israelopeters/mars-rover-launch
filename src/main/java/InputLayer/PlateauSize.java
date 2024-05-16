48543663675ed0e7b0e9b296f2efc638f2a0b184
israelopeters <israelopeters@gmail.com>
2024-05-16 13:46:29 +0100

    feat: initial commit

---
+++ src/main/java/InputLayer/PlateauSize.java
@@ -0,0 +1,11 @@
+package InputLayer;
+
+public class PlateauSize {
+    private final int x;
+    private final int y;
+
+    public PlateauSize(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
